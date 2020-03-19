50d8a5e727628e17150587f7a5a6b6d1b860d9b1
gerhardfischer <62342384+gerhardfischer@users.noreply.github.com>
2020-03-19 14:28:26 +0100

    Update InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,19 +1,19 @@
 package org.openshift;
+import java.util.HashMap;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
-import java.util.Random;
-
-public class InsultGenerator {
-	public String generateInsult() {
-		String words[][] = {{"artless", "bawdy", "beslubbering"}, {"base-court", "bat-fowling", "beef-witted"}, {"apple-john", "baggage", "barnacle"}};
-		String vowels = "aeiou";
-		String article = "an";
-		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
-		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
-		String noun = words[2][new Random().nextInt(words[2].length)];
-		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
-			article = "a";
-		}
-		return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
+@RequestScoped
+@Path("/insult")
+public class InsultResource {
+	@GET()
+	@Produces("application/json")
+	public HashMap<String,String> getInsult() {
+		HashMap<String, String> theInsult = new HashMap<String, String>();
+		theInsult.put("insult", new InsultGenerator().generateInsult());
+		return theInsult;
 	}
-
 }
+
